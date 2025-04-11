ddc7d5140a109515819e979e296ba76c84669136
pewcia <ewelina.serafin996@gmail.com>
2025-04-11 18:57:48 +0200

    API integration

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ function refreshWeather(response) {
   windSpeedElement.innerHTML = `${response.data.wind.speed} km/h`;
   temperatureElement.innerHTML = Math.round(temperature);
   iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-app-icon" />`;
+  getForecast(response.data.city);
 }
 
 function formatDate(date) {
@@ -52,35 +53,48 @@ function handleSearchSubmit(event) {
   searchCity(searchInput.value);
 }
 
-function displayForecast() {
-    let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
-    let forecastHtml = "";
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  return days[date.getDay()];
+}
 
-days.forEach(function (day) {
-    forecastHtml = 
-    forecastHtml +
+function getForecast(city) {
+  let apiKey = "bb203f4a92e7t89f33c44201bd2fbfoa";
+  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}`;
+  axios(apiUrl).then(displayForecast);
+}
 
-    `<div class="weather-forecast-day">
-      <div class="weather-forecast-date">${day}</div> 
-         <div class="weather-forecast-icon"> ⛅</div> 
+function displayForecast(response) {
+  console.log(response.data);
+
+  let forecastHtml = "";
+
+  response.data.daily.forEach(function (day, index) {
+    if (index < 5) {
+      forecastHtml =
+        forecastHtml +
+        `<div class="weather-forecast-day">
+      <div class="weather-forecast-date">${formatDay(day.time)}</div> 
+    <img src="${day.condition.icon_url}" class="weather-forecast-icon" />
         <div class="weather-forecast-temperatures">
         <div class="weather-forecast-temperature">
-                 <strong>15°C </strong>
+                 <strong>${Math.round(day.temperature.maximum)}°C </strong>
                 </div>
-         <div class="weather-forecast-temperature"> 10°C </div> 
+         <div class="weather-forecast-temperature"> ${Math.round(
+           day.temperature.minimum
+         )}°C </div> 
      </div>
     </div>
     `;
-});
+    }
+  });
 
-let forecastElement = document.querySelector("#forecast");
-forecastElement.innerHTML = forecastHtml;
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = forecastHtml;
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
-searchCity("Paris");
-
-displayForecast();
-        
+searchCity("Tilburg");
