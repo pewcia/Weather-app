a8ca121eb8830e9720f42678c9188f31b8d9875b
pewcia <ewelina.serafin996@gmail.com>
2025-04-11 17:13:53 +0200

    Inject the forecast HTML from JavaScript

--- index.js
+++ index.js
@@ -52,7 +52,35 @@ function handleSearchSubmit(event) {
   searchCity(searchInput.value);
 }
 
+function displayForecast() {
+    let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
+    let forecastHtml = "";
+
+days.forEach(function (day) {
+    forecastHtml = 
+    forecastHtml +
+
+    `<div class="weather-forecast-day">
+      <div class="weather-forecast-date">${day}</div> 
+         <div class="weather-forecast-icon"> ⛅</div> 
+        <div class="weather-forecast-temperatures">
+        <div class="weather-forecast-temperature">
+                 <strong>15°C </strong>
+                </div>
+         <div class="weather-forecast-temperature"> 10°C </div> 
+     </div>
+    </div>
+    `;
+});
+
+let forecastElement = document.querySelector("#forecast");
+forecastElement.innerHTML = forecastHtml;
+}
+
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
 searchCity("Paris");
+
+displayForecast();
+        
